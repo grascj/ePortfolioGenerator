1bfda2fcba77cd0e291fd37d6b34372bcdc059f4
cgmp <cgmp@10.1.1.4>
2015-11-03 22:20:33 -0500

    adding the Model Classes

--- src/epg/model/Component.java
+++ src/epg/model/Component.java
@@ -22,6 +22,6 @@ public abstract class Component {
            
     public abstract void editPrompt();
     public abstract void display();
-    public abstract void htmlify();    
+    public abstract String htmlify();    
     
 }
